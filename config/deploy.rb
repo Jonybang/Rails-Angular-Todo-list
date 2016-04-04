f75384442bfdedac4827212472518d2a704c2927
Jonybang <jonybang@mail.ru>
2016-04-05 00:12:41 +1000

    init

--- config/deploy.rb
+++ config/deploy.rb
@@ -74,6 +74,17 @@
 		  on roles(:app), in: :sequence, wait: 5 do
 		    invoke 'puma:restart'
 		  end
+    end
+
+		desc 'Runs rake db:seed'
+		task :seed => [:set_rails_env] do
+			on primary fetch(:migration_role) do
+				within release_path do
+					with rails_env: fetch(:rails_env) do
+						execute :rake, "db:seed"
+					end
+				end
+			end
 		end
 
 		before :starting,     :check_revision
